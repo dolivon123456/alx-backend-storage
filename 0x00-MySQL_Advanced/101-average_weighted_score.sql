49cfb5e3db74899c278584f4aad859133b04ca5a
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2023-07-19 13:44:05 +0100

    Update 101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,42 +1,17 @@
--- This is a MySQL script that creates a stored procedure
--- ComputeAverageWeightedScoreForUsers that computes and
--- store the average weighted score for all students.
--- Tables
--- 		users(id, name, average_score)
---		projects(id,, name, weight)
--- 		corrections(user_id, project_id, score)
+-- creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers$$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	DECLARE cummulativeWeightedSum, weightedSum, weightedAverage INT;
-    DECLARE userId INT;
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE user_cursor CURSOR FOR
-    SELECT id
-    FROM users;
-    DECLARE
-		CONTINUE HANDLER FOR NOT FOUND
-    SET done = TRUE;
-    OPEN user_cursor;
-    user_loop:
-    LOOP
-		FETCH user_cursor INTO userId;
-    IF done THEN LEAVE user_loop;
-    END IF;
-	SELECT sum(score * (SELECT weight FROM projects WHERE id = project_id))
-		INTO cummulativeWeightedSum
-		FROM corrections
-		WHERE user_id = userId;
-	SELECT sum((SELECT weight FROM projects WHERE id = project_id))
-		INTO weightedSum
-		FROM corrections
-		WHERE user_id = userId;
-	SET weightedAverage = cummulativeWeightedSum / weightedSum;
-	UPDATE users 
-		SET average_score = weightedAverage
-		WHERE id = userId;
-	END LOOP;
-    CLOSE user_cursor;
-END$$
+    UPDATE users AS U, 
+        (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
+        FROM users AS U 
+        JOIN corrections as C ON U.id=C.user_id 
+        JOIN projects AS P ON C.project_id=P.id 
+        GROUP BY U.id)
+    AS WA
+    SET U.average_score = WA.w_avg 
+    WHERE U.id=WA.id;
+END
+$$
 DELIMITER ;
